9475dcf93505602bb4b32be28fc2d0f88928811f
Andrea <andrea.l.martens@gmail.com>
2022-03-23 18:11:55 -0400

    updated compost at home project

---
+++ compost-at-home/story_content/5xNw9GKFT4n_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.60.26947.0%0D%0A%0D%0A00:00:00.064%20--%3E%2000:00:02.945%0D%0ASelect%20a%20common%20challenge%20to%20learn%20about%20a%20solution.%0D%0A%0D%0A"}')
